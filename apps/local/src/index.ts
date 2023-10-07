afca89ef50a43ba11eb7e252029c58d4d76fb8d2
Quinn Penney <elijahpenney@outlook.com>
2023-10-06 19:15:07 -0700

    Updated to make run dev work.

--- apps/local/src/index.ts
+++ apps/local/src/index.ts
@@ -1,48 +1,7 @@
-import { app, BrowserWindow, net } from 'electron';
+import { app, BrowserWindow } from 'electron';
 import path from 'path';
 
 let mainWindow: BrowserWindow | null = null;
-
-const checkLocalhostAvailable = async () => {
-  return new Promise<boolean>((resolve) => {
-    const request = net.request('http://127.0.0.1:5173');
-
-    request.on('response', (response) => {
-      console.log(response);
-      // Check for the expected response status
-      if (response.statusCode === 200) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    });
-
-    request.on('error', () => {
-      resolve(false);
-    });
-
-    request.end();
-  });
-};
-
-const loadAppOrFallback = async () => {
-  const isAvailable = await checkLocalhostAvailable();
-
-  if (isAvailable) {
-    await mainWindow?.loadURL('http://127.0.0.1:5173');
-  } else {
-    const fallbackPath = path.join(__dirname, 'public', 'loading.html');
-    const fallbackUrl = new URL(`file://${fallbackPath}`);
-    await mainWindow?.loadURL(fallbackUrl.toString());
-
-    const intervalId = setInterval(async () => {
-      if (await checkLocalhostAvailable()) {
-        clearInterval(intervalId);
-        await mainWindow?.loadURL('http://127.0.0.1:5173');
-      }
-    }, 1000);
-  }
-};
 
 const createWindow = async () => {
   mainWindow = new BrowserWindow({
@@ -53,7 +12,11 @@ const createWindow = async () => {
     },
   });
 
-  await mainWindow?.loadFile(path.join(__dirname, 'app', 'index.html'));
+  if (process.env.NODE_ENV === 'development') {
+    await mainWindow?.loadURL('http://localhost:5173');
+  } else {
+    await mainWindow?.loadFile(path.join(__dirname, 'app', 'index.html'));
+  }
 
   // Open devtools in a separate window
   mainWindow.webContents.openDevTools({ mode: 'detach' });
